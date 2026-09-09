383da438e1175dc7ca591081929e0b8e86bb5221
Gaaramiro <gaaramiro@live.com>
2026-09-09 16:06:04 -0300

    adicionado pagina de produtos. fetch de produto por parametro utilizando id.

--- src/Components/Produto.jsx
+++ src/Components/Produto.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import Head from '../Components/Head';
+import styles from '../Components/Produto.module.css';
 
 function Produto() {
-  return <div></div>;
+  const [produto, setProduto] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(null);
+  const { id } = useParams();
+  React.useEffect(() => {
+    async function fetchProduto(url) {
+      try {
+        setLoading(true);
+        const res = await fetch(url);
+        const json = await res.json();
+        setProduto(json);
+        console.log(produto);
+      } catch (erro) {
+        setError('Um erro ocorreu');
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
+  }, [id]);
+
+  if (loading) return <div className="loading"></div>;
+  if (error) return <p>{error}</p>;
+  if (produto === null) return null;
+  return (
+    <section className={`${styles.produto} animeLeft`}>
+      <Head title={produto.nome} description={produto.descricao} />
+      <div>
+        {produto.fotos.map((foto) => (
+          <img key={foto.src} src={foto.src} alt={foto.titulo} />
+        ))}
+      </div>
+      <div>
+        <h2>{produto.nome}</h2>
+        <span>R$ {produto.preco}</span>
+        <p>{produto.descricao}</p>
+      </div>
+    </section>
+  );
 }
 
 export default Produto;
