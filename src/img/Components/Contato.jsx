5b5d3b99317857eaa8d849e84ffc920be76a5ef5
Gaaramiro <gaaramiro@live.com>
2026-09-09 10:17:55 -0300

    Adicionado Components

---
+++ src/img/Components/Contato.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Contato() {
+  return <div></div>;
+}
+
+export default Contato;
---
+++ src/img/Components/Produtos.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Produtos() {
+  return <div></div>;
+}
+
+export default Produtos;
